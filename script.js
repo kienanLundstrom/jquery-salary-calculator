c6762818dac36f6bc8ca529fabf2f7eb685e65bd
Kienan Lundstrom <keen.lundy@gmail.com>
2019-09-02 11:49:40 -0500

    adding employee to array by input and empty input field(not working)

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 $(document).ready(readyNow);
 
+let newEmployees = [];
+
+function Employee( firstName, lastName, iD, title, annualSalary){
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.iD = iD,
+    this.title = title,
+    this.annualSalary = annualSalary
+}
+
 function newEmployeeInput(){
-    console.log ('click listner')
+    let newEmployeeIn = new Employee(
+      $('#firsNameInput'),
+      $('#lastNameInput'),
+      $('#employeeIdInput'), 
+      $('#employeeTitleInput'), 
+      $('#annualSalaryInput'))
+    emptyInput();
+}
+function emptyInput(){
+    $('#firsNameInput').empty();
+    $('#lastNameInput').empty();
+    $('#employeeIdInput').empty();
+     $('#employeeTitleInput').empty();
+      $('#annualSalaryInput').empty();
 }
 
 function readyNow(){
